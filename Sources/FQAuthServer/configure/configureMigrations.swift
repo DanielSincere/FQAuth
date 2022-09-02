2966d68632da87a3f0353db2af259767d61595fb
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2022-09-02 14:46:22 -0400

    remove automigrate

--- Sources/FQAuthServer/configure/configureMigrations.swift
+++ Sources/FQAuthServer/configure/configureMigrations.swift
@@ -7,18 +7,16 @@ extension Application {
     for i in AllMigrations.allCases {
       self.migrations.add(i.migration, to: .psql)
     }
-
-
-    switch self.environment {
-    case .testing:
-      try self.autoRevert().wait()
-      try self.autoMigrate().wait()
-    case .development:
-      break
-    case .production:
-      break
-    default:
-      break
-    }
+//    switch self.environment {
+//    case .testing:
+//      try self.autoRevert().wait()
+//      try self.autoMigrate().wait()
+//    case .development:
+//      break
+//    case .production:
+//      break
+//    default:
+//      break
+//    }
   }
 }
