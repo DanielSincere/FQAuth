addc96a6304bc1cdf00839bf63a7c9439524a45f
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2022-12-17 18:58:23 -0500

    wip

--- Tests/FQAuthServerTests/Apple/SIWAAuthorizeTests.swift
+++ Tests/FQAuthServerTests/Apple/SIWAAuthorizeTests.swift
@@ -9,13 +9,7 @@ final class SIWAAuthorizeTests: XCTestCase {
   var app: Application!
   override func setUpWithError() throws {
     self.app = Application(.testing)
-
-
-    app.services.siwaClient.use { application in
-      var fake = FakeSIWAClient(eventLoop: application.eventLoopGroup.next())
-      fake.generateRefreshTokenStub = AppleTokenResponse(access_token: "token", expires_in: 3600, id_token: "id_token", refresh_token: "refresh_token", token_type: "type")
-      return fake
-    }
+    try app.configure()
     app.services.siwaVerifier.use { application in
       var fake = FakeSIWAVerifier(eventLoop: application.eventLoopGroup.next())
       
@@ -25,18 +19,17 @@ final class SIWAAuthorizeTests: XCTestCase {
       fake.verifyStub = stub
       return fake
     }
-    try app.configure()
+    
+    app.services.siwaClient.use { application in
+      var fake = FakeSIWAClient(eventLoop: application.eventLoopGroup.next())
+      fake.generateRefreshTokenStub = AppleTokenResponse(access_token: "token", expires_in: 3600, id_token: "id_token", refresh_token: "refresh_token", token_type: "bearer")
+      return fake
+    }
+    
+
+    
+
   }
-  
-//  lazy var verifierFake: SIWAVerifier = {
-//    var fake = FakeSIWAVerifier(eventLoop: app.eventLoopGroup.next())
-//
-//    let tokenResponse = try! JSONDecoder().decode(AppleTokenResponse.self, from: ByteBuffer(string: AppleFixtures.successfulSiwaSignInBody))
-//
-//    let stub = try! JWTSigners().unverified(tokenResponse.id_token,as: AppleIdentityToken.self)
-//    fake.verifyStub = stub
-//    return fake
-//  }()
   
   override func tearDownWithError() throws {
     app.shutdown()
