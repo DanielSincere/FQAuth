0b04d4e60802579508f3e035c1784f7be1a246f3
Full Queue Developer <FullQueueDeveloper@fullqueuedeveloper.com>
2023-01-17 17:51:13 -0500

    refactor test for readability, reducing duplication

--- Tests/FQAuthServerTests/Apple/SIWAServerNotificationRequestTests.swift
+++ Tests/FQAuthServerTests/Apple/SIWAServerNotificationRequestTests.swift
@@ -3,19 +3,20 @@ import XCTest
 import Foundation
 import Vapor
 import JWTKit
+import Queues
 
 final class SIWAServerNotificationRequestTests: XCTestCase {
 
-  var app: Application!
-
   let appleUserId = "820417.faa325acbc78e1be1668ba852d492d8a.0219"
 
+  var app: Application!
   override func setUpWithError() throws {
     self.app = Application(.testing)
     try app.configure()
     try app.resetDatabase()
 
-    try SIWASignUpRepo(application: app).createTestUser(appleUserId: appleUserId)
+    try SIWASignUpRepo(application: app)
+      .createTestUser(appleUserId: appleUserId)
   }
 
   override func tearDownWithError() throws {
@@ -24,85 +25,59 @@ final class SIWAServerNotificationRequestTests: XCTestCase {
 
   func testConsentRevoked() throws {
 
-    let notification = SIWAServerNotification(
-      iss: IssuerClaim(value: "https://appleid.apple.com"),
-      aud: AudienceClaim(stringLiteral: "com.fullqueuedeveloper.FQAuth"),
-      iat: IssuedAtClaim(value: Date()),
-      jti: IDClaim(value: "abede67890"),
-      events: try .init(string: """
-          {
-            \"type\":\"consent-revoked\",
-            \"sub\":\"\(appleUserId)\",
-            \"event_time\":1670016125295
-          }
-          """))
+    let notification = try buildNotification(for: """
+      {
+        \"type\":\"consent-revoked\",
+        \"sub\":\"\(appleUserId)\",
+        \"event_time\":1670016125295
+      }
+      """)
 
-    let jwt: String = try app.jwt.signers.sign(notification)
-    let notifyBody = SIWAController.NotifyBody(payload: jwt)
-    let notificationBodyJson = try JSONEncoder().encode(notifyBody)
-
-    try app.test(.POST, "/api/siwa/notify",
-                 headers: HTTPHeaders([("content-type", "application/json")]),
-                 body: ByteBuffer(data: notificationBodyJson)) { response in
-
-      XCTAssertEqual(response.status, .ok)
-
-      let nextJobId = try XCTUnwrap(app.queues.queue.pop().wait())
-      let nextJob = try app.queues.queue.get(nextJobId).wait()
-      let payload: String = try JSONDecoder().decode(String.self, from: ByteBuffer(bytes: nextJob.payload))
-      XCTAssertEqual(nextJob.jobName, "ConsentRevokedJob")
+    try postNotification(notification, callback: { (jobData, payload: String) in
+      XCTAssertEqual(jobData.jobName, "ConsentRevokedJob")
       XCTAssertEqual(payload, appleUserId)
-    }
+    })
   }
 
   func testAccountDelete() throws {
-    let notification = SIWAServerNotification(
-      iss: IssuerClaim(value: "https://appleid.apple.com"),
-      aud: AudienceClaim(stringLiteral: "com.fullqueuedeveloper.FQAuth"),
-      iat: IssuedAtClaim(value: Date()),
-      jti: IDClaim(value: "abede67890"),
-      events: try .init(string: """
-          {
-            \"type\":\"account-delete\",
-            \"sub\":\"\(appleUserId)\",
-            \"event_time\":1670016125295
-          }
-          """))
+    let notification = try buildNotification(for: """
+      {
+        \"type\":\"account-delete\",
+        \"sub\":\"\(appleUserId)\",
+        \"event_time\":1670016125295
+      }
+      """)
 
-    let jwt: String = try app.jwt.signers.sign(notification)
-    let notifyBody = SIWAController.NotifyBody(payload: jwt)
-    let notificationBodyJson = try JSONEncoder().encode(notifyBody)
-
-    try app.test(.POST, "/api/siwa/notify",
-                 headers: HTTPHeaders([("content-type", "application/json")]),
-                 body: ByteBuffer(data: notificationBodyJson)) { response in
-
-      XCTAssertEqual(response.status, .ok)
-
-      let nextJobId = try XCTUnwrap(app.queues.queue.pop().wait())
-      let nextJob = try app.queues.queue.get(nextJobId).wait()
-      let payload: String = try JSONDecoder().decode(String.self, from: ByteBuffer(bytes: nextJob.payload))
-      XCTAssertEqual(nextJob.jobName, "SIWAAccountDeletedJob")
+    try postNotification(notification) { (jobData, payload: String) in
+      XCTAssertEqual(jobData.jobName, "SIWAAccountDeletedJob")
       XCTAssertEqual(payload, appleUserId)
     }
   }
 
   func testEmailEnabled() throws {
-    let notification = SIWAServerNotification(
-      iss: IssuerClaim(value: "https://appleid.apple.com"),
-      aud: AudienceClaim(stringLiteral: "com.fullqueuedeveloper.FQAuth"),
-      iat: IssuedAtClaim(value: Date()),
-      jti: IDClaim(value: "abede67890"),
-      events: try .init(string: """
-          {
-            \"type\":\"email-enabled\",
-            \"sub\":\"\(appleUserId)\",
-            \"event_time\":1673016125295,
-            \"email\": "new@example.nyc",
-            \"is_private_email\": "true"
-          }
-          """))
+    let notification = try buildNotification(for: """
+      {
+        \"type\":\"email-enabled\",
+        \"sub\":\"\(appleUserId)\",
+        \"event_time\":1673016125295,
+        \"email\": "new@example.nyc",
+        \"is_private_email\": "true"
+      }
+      """)
 
+    try postNotification(notification) { (jobData: JobData,
+                                          payload: EmailEnabledJob.Payload) in
+
+      XCTAssertEqual(jobData.jobName, "EmailEnabledJob")
+      XCTAssertEqual(payload.appleUserID, appleUserId)
+      XCTAssertEqual(payload.newEmail, "new@example.nyc")
+    }
+  }
+
+  private func postNotification<T: Decodable>(_ notification: SIWAServerNotification,
+                                              file: StaticString = #filePath,
+                                              line: UInt = #line,
+                                              callback: (JobData, T) throws -> ()) throws {
     let jwt: String = try app.jwt.signers.sign(notification)
     let notifyBody = SIWAController.NotifyBody(payload: jwt)
     let notificationBodyJson = try JSONEncoder().encode(notifyBody)
@@ -110,15 +85,22 @@ final class SIWAServerNotificationRequestTests: XCTestCase {
     try app.test(.POST, "/api/siwa/notify",
                  headers: HTTPHeaders([("content-type", "application/json")]),
                  body: ByteBuffer(data: notificationBodyJson)) { response in
-      
-      XCTAssertEqual(response.status, .ok)
-      
+
+      XCTAssertEqual(response.status, .ok, file: file, line: line)
+
       let nextJobId = try XCTUnwrap(app.queues.queue.pop().wait())
       let nextJob = try app.queues.queue.get(nextJobId).wait()
-      let payload: EmailEnabledJob.Payload = try JSONDecoder().decode(EmailEnabledJob.Payload.self, from: ByteBuffer(bytes: nextJob.payload))
-      XCTAssertEqual(nextJob.jobName, "EmailEnabledJob")
-      XCTAssertEqual(payload, .init(newEmail: "new@example.nyc",
-                                    appleUserID: appleUserId))
+      let payload: T = try JSONDecoder().decode(T.self, from: ByteBuffer(bytes: nextJob.payload))
+      try callback(nextJob, payload)
     }
+  }
+
+  private func buildNotification(for event: String) throws -> SIWAServerNotification {
+    SIWAServerNotification(
+      iss: IssuerClaim(value: "https://appleid.apple.com"),
+      aud: AudienceClaim(stringLiteral: "com.fullqueuedeveloper.FQAuth"),
+      iat: IssuedAtClaim(value: Date()),
+      jti: IDClaim(value: "abede67890"),
+      events: try .init(string: event))
   }
 }
