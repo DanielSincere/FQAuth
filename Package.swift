cb7940ea6c0829e21388173aadf3e41da665d765
Full Queue Developer <FullQueueDeveloper@FullQueueDeveloper.com>
2023-03-04 23:28:28 -0500

    upgrade to FQAuthMiddleware 0.1.2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   ],
   dependencies: [
     
-    .package(url: "https://github.com/FullQueueDeveloper/FQAuthMiddleware.git", from: "0.1.1"),
+    .package(url: "https://github.com/FullQueueDeveloper/FQAuthMiddleware.git", from: "0.1.2"),
     
     .package(url: "https://github.com/vapor/vapor.git", from: "4.65.1"),
     .package(url: "https://github.com/vapor/jwt.git", from: "4.2.1"),
